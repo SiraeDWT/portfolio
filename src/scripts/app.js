74e33b1f4c5a2e706bcab3fcd8e1f174899f6513
Dylan <dylanv.dwt@hotmail.com>
2024-05-01 14:11:24 +0200

    Add new canvas for first section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -387,13 +387,82 @@ const drawArea = canvas.getContext("2d");
 const particleAmountDesktop = 30;
 const particleAmountMobile = 10;
 
-displayCanvas(canvas, drawArea, particleAmountDesktop);
+// displayCanvas(canvas, drawArea, particleAmountDesktop);
 displayCanvas(canvasContact, drawAreaContact, particleAmountDesktop);
 
 if (window.matchMedia('(min-width: 640px)').matches) {
-    displayCanvas(canvas, drawArea, particleAmountDesktop);
+    // displayCanvas(canvas, drawArea, particleAmountDesktop);
     displayCanvas(canvasContact, drawAreaContact, particleAmountDesktop);
 } else {
-    displayCanvas(canvas, drawArea, particleAmountMobile);
+    // displayCanvas(canvas, drawArea, particleAmountMobile);
     displayCanvas(canvasContact, drawAreaContact, particleAmountMobile);
 }
+
+
+
+
+
+
+
+
+// TEST CANVAS FIRST SECTION
+
+var can = document.getElementById("canvas");
+var ctx = can.getContext("2d");
+can.width = window.innerWidth;
+can.height = window.innerHeight;
+can.style.background = "#060D19";
+var p = []
+function Clear() {
+ctx.fillStyle="rgba(0,0,0,0.12)"
+ctx.fillRect(0,0,can.width,can.height);
+} 
+function particle(x,y,speed,c) {
+this.x = x
+this.y = y
+this.speed = speed
+this.upd = function() {
+ctx.strokeStyle = c;
+ctx.lineWidth = 1
+ctx.lineCap = "round"
+ctx.beginPath()
+ctx.moveTo(this.x,this.y)
+this.x += this.speed.x
+this.y += this.speed.y
+ctx.lineTo(this.x,this.y)
+ctx.stroke()
+ this.ang = Math.atan2(this.speed.y,this.speed.x)
+this.mag = Math.sqrt(this.speed.x**2 + this.speed.y**2)
+var op = [this.ang+Math.PI/4,this.ang-Math.PI/4]
+var ch = Math.floor(Math.random()*op.length)
+if(Math.random() < 0.05) {
+this.speed.x = Math.cos(op[ch])*this.mag
+this.speed.y = Math.sin(op[ch])*this.mag
+}
+}
+}
+var speed = 15
+var period = 3000
+function pulse() {
+setTimeout(pulse,period)
+var h = Math.random()*(210-150) + 150
+for(var i = 0; i < 56; i++) {
+p.push(new particle(can.width/2,0,
+{
+x:Math.cos(i/8*2*Math.PI)*speed,
+y:Math.sin(i/8*2*Math.PI)*speed
+},"#ffdd00"))
+}
+}
+function gameMove(){
+requestAnimationFrame(gameMove)
+Clear()
+for(var i = 0; i < p.length; i++) {
+p[i].upd();
+if(p[i].x < 0 || p[i].x > can.width || p[i].y < 0 || p[i].y > can.height) {
+p.splice(i,1)
+}
+}
+}
+pulse()
+gameMove()
